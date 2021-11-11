5439fcd2f049504311a1657178b27567c6b275c0
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-11-11 11:52:39 +0300

    0. Spring DataSource [#6878 #227041]

--- src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
@@ -2,6 +2,7 @@ package ru.job4j.accident.repository;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -16,10 +17,24 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
-//@Repository
+@Repository
 public class AccidentJdbcTemplate implements AccidentRepository {
 
     private final JdbcTemplate jdbc;
+
+    private RowMapper<Accident> accidentRowMapper = (rs, row) -> {
+        Accident accident = new Accident();
+        accident.setId(rs.getInt("a_id"));
+        accident.setName(rs.getString("a_name"));
+        accident.setText(rs.getString("a_text"));
+        accident.setAddress(rs.getString("a_address"));
+        accident.setCreated(rs.getTimestamp("a_created"));
+        AccidentType accidentType = new AccidentType();
+        accidentType.setId(rs.getInt("t_id"));
+        accidentType.setName(rs.getString("t_name"));
+        accident.setType(accidentType);
+        return accident;
+    };
 
     public AccidentJdbcTemplate(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
@@ -28,46 +43,54 @@ public class AccidentJdbcTemplate implements AccidentRepository {
     @Override
     public Accident saveOrUpdateAccident(Accident accident) {
         if (accident.getId() == 0) {
-            final String INSERT_SQL = "insert into accidents (name, text, address, type_id, created) "
-                    + "values (?, ?, ?, ?, ?)";
-            KeyHolder keyHolder = new GeneratedKeyHolder();
-            jdbc.update(connection -> {
-                PreparedStatement ps = connection.prepareStatement(INSERT_SQL, new String[]{"id"});
-                ps.setString(1, accident.getName());
-                ps.setString(2, accident.getText());
-                ps.setString(3, accident.getAddress());
-                ps.setInt(4, accident.getType().getId());
-                ps.setTimestamp(5, new Timestamp(accident.getCreated().getTime()));
-                return ps;
-            }, keyHolder);
-            accident.setId(keyHolder.getKey().intValue());
-            for (Rule rule : accident.getRules()) {
-                jdbc.update("insert into accidents_rules (rule_id, accident_id) "
-                                + "values (?, ?)",
-                        rule.getId(),
-                        accident.getId());
-            }
+            save(accident);
         } else {
-            final String INSERT_SQL = "update accidents set name = ?, text = ?, address = ?, type_id = ? where id = ?";
-            jdbc.update(INSERT_SQL,
-                    accident.getName(),
-                    accident.getText(),
-                    accident.getAddress(),
-                    accident.getType().getId(),
-                    accident.getId());
-
-            jdbc.update(
-                    "delete from accidents_rules where accident_id = ?",
-                    accident.getId());
-
-            for (Rule rule : accident.getRules()) {
-                jdbc.update("insert into accidents_rules (rule_id, accident_id) "
-                                + "values (?, ?)",
-                        rule.getId(),
-                        accident.getId());
-            }
+            update(accident);
         }
         return accident;
+    }
+
+    private void update(Accident accident) {
+        final String INSERT_SQL = "update accidents set name = ?, text = ?, address = ?, type_id = ? where id = ?";
+        jdbc.update(INSERT_SQL,
+                accident.getName(),
+                accident.getText(),
+                accident.getAddress(),
+                accident.getType().getId(),
+                accident.getId());
+
+        jdbc.update(
+                "delete from accidents_rules where accident_id = ?",
+                accident.getId());
+
+        for (Rule rule : accident.getRules()) {
+            jdbc.update("insert into accidents_rules (rule_id, accident_id) "
+                            + "values (?, ?)",
+                    rule.getId(),
+                    accident.getId());
+        }
+    }
+
+    private void save(Accident accident) {
+        final String INSERT_SQL = "insert into accidents (name, text, address, type_id, created) "
+                + "values (?, ?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbc.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(INSERT_SQL, new String[]{"id"});
+            ps.setString(1, accident.getName());
+            ps.setString(2, accident.getText());
+            ps.setString(3, accident.getAddress());
+            ps.setInt(4, accident.getType().getId());
+            ps.setTimestamp(5, new Timestamp(accident.getCreated().getTime()));
+            return ps;
+        }, keyHolder);
+        accident.setId(keyHolder.getKey().intValue());
+        for (Rule rule : accident.getRules()) {
+            jdbc.update("insert into accidents_rules (rule_id, accident_id) "
+                            + "values (?, ?)",
+                    rule.getId(),
+                    accident.getId());
+        }
     }
 
     @Override
@@ -77,20 +100,8 @@ public class AccidentJdbcTemplate implements AccidentRepository {
                 + "t.id as t_id, t.name as t_name "
                 + "from accidents as a left join types as t on a.type_id = t.id";
         List<Accident> accidents = new ArrayList<>();
-        accidents = jdbc.query(SELECT_SQL,
-                (rs, row) -> {
-                    Accident accident = new Accident();
-                    accident.setId(rs.getInt("a_id"));
-                    accident.setName(rs.getString("a_name"));
-                    accident.setText(rs.getString("a_text"));
-                    accident.setAddress(rs.getString("a_address"));
-                    accident.setCreated(rs.getTimestamp("a_created"));
-                    AccidentType accidentType = new AccidentType();
-                    accidentType.setId(rs.getInt("t_id"));
-                    accidentType.setName(rs.getString("t_name"));
-                    accident.setType(accidentType);
-                    return accident;
-                });
+
+        accidents = jdbc.query(SELECT_SQL, accidentRowMapper);
         for (Accident accident : accidents) {
             List<Rule> rules = jdbc.query("select r.id as r_id, r.name as r_name from rules r "
                             + "left join accidents_rules ar on r.id = ar.rule_id where ar.accident_id = ?",
@@ -111,20 +122,7 @@ public class AccidentJdbcTemplate implements AccidentRepository {
                 + "a.address as a_address, a.type_id as a_type_id, a.created as a_created, "
                 + "t.id as t_id, t.name as t_name "
                 + "from accidents a left join types t on a.type_id = t.id where a.id = ?";
-        Accident accident = jdbc.queryForObject(SELECT_SQL,
-                (rs, row) -> {
-                    Accident acc = new Accident();
-                    acc.setId(rs.getInt("a_id"));
-                    acc.setName(rs.getString("a_name"));
-                    acc.setText(rs.getString("a_text"));
-                    acc.setAddress(rs.getString("a_address"));
-                    acc.setCreated(rs.getTimestamp("a_created"));
-                    AccidentType accidentType = new AccidentType();
-                    accidentType.setId(rs.getInt("t_id"));
-                    accidentType.setName(rs.getString("t_name"));
-                    acc.setType(accidentType);
-                    return acc;
-                }, id);
+        Accident accident = jdbc.queryForObject(SELECT_SQL, accidentRowMapper, id);
         if (accident == null) {
             throw new IllegalArgumentException(String.format("Заявления с id: %s не существует!", id));
         }
